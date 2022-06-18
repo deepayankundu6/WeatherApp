9ffaf0931ab522ab8373a578b7038567cda044fa
Deepayan Kundu <deepayankundu6@gmail.com>
2022-06-18 12:34:03 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import { getWeatherData } from "./weatherCall";
+import { getWeatherData, getIcon } from "./weatherCall";
 import { useEffect, useState } from 'react';
+
 function App() {
-  const [weatherData, setWeatherData] = useState(" ");
+  const [weatherData, setWeatherData] = useState(null);
+  const [weatherIcon, setWeatherIcon] = useState(null)
   const [cityName, setcityName] = useState(" ");
   useEffect(() => {
     // async function fetchData() {
@@ -15,24 +17,46 @@ function App() {
   const SubmitForm = (event) => {
     event.preventDefault();
     fetchData(event.target[0].value);
+
   }
   const fetchData = async (cityName) => {
     let ApiResponse = await getWeatherData(cityName);
     setWeatherData(ApiResponse);
+    const icon = await getIcon(ApiResponse.weather[0].icon);
+    setWeatherIcon(icon);
   }
 
   return (
+    <div>
+      <div className="row">
+        <form className="col s12" onSubmit={(e) => { SubmitForm(e); }}>
+          <div className="row">
+            <div className="input-field col s6 center">
+              <input type="text" onChange={(e) => { setcityName(e.target.value) }} />
+              <label htmlFor="first_name" id="first_name" type="text" className="validate"><i className="fa-solid fa-magnifying-glass"></i>  City Name</label>
+            </div>
+          </div>
+        </form>
+      </div>
 
-    <div className="row">
-      <form className="col s12" onSubmit={(e) => { SubmitForm(e); setcityName(" "); }}>
-        <div className="row">
-          <div className="input-field col s6">
-            <input type="text" onChange={(e) => { setcityName(e.target.value) }} />
-            <label htmlFor="first_name" id="first_name" type="text" className="validate">City Name</label>
+      {weatherData && < div className="row">
+        <div className="col s12 m7">
+          <div className="card">
+            <div className="card-image image">
+              <img src={weatherIcon} />
+            </div>
+            <span className="card-title title">{cityName}</span>
+            <div className="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+                I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div className="card-action">
+            </div>
           </div>
         </div>
-      </form>
+      </div>}
     </div>
+
 
   );
 }
--- src/weatherCall.js
+++ src/weatherCall.js
@@ -1,5 +1,6 @@
 const APIKey = "b1ee6b0ab2f054e7a60a315e0f475813";
-const domin = "https://api.openweathermap.org"
+const domin = "https://api.openweathermap.org";
+
 const getWeatherData = async (cityName) => {
     console.log("Fetching weather for: ", cityName);
     const path = `/data/2.5/weather?q=${cityName.toLowerCase()}&appid=${APIKey}`;
@@ -8,4 +9,11 @@ const getWeatherData = async (cityName) => {
     return weatherData;
 }
 
-export { getWeatherData };
+const getIcon = async (icon) => {
+    const url = `http://openweathermap.org/img/w`;
+    let weatherIcon = await fetch(url + `/${icon}.png`);
+    weatherIcon = await weatherIcon.blob();
+    weatherIcon = URL.createObjectURL(weatherIcon);
+    return weatherIcon;
+}
+export { getWeatherData, getIcon };
