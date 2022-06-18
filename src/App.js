eb7b2b33c6ad2d0d38f02349a07e9fa8a5d8d32a
Deepayan Kundu <deepayankundu6@gmail.com>
2022-06-18 13:36:07 +0530

    efef

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [weatherIcon, setWeatherIcon] = useState(null);
   const [City, setCity] = useState(null);
-  const [cityName, setcityName] = useState(" ");
+  const [cityName, setcityName] = useState(null);
+  const [Unit, setunit] = useState("metric");
   useEffect(() => {
     // async function fetchData() {
     //   ApiResponse = await getWeatherData(cityName);
@@ -21,11 +22,36 @@ function App() {
 
   }
   const fetchData = async (cityName) => {
-    let ApiResponse = await getWeatherData(cityName);
-    setWeatherData(ApiResponse);
-    const icon = await getIcon(ApiResponse.weather[0].icon);
-    setWeatherIcon(icon);
-    setCity(cityName);
+
+    if (cityName) {
+      let ApiResponse = await getWeatherData(cityName, Unit);
+      setWeatherData(ApiResponse);
+      const icon = await getIcon(ApiResponse.weather[0].icon);
+      setWeatherIcon(icon);
+      setCity(cityName);
+    }
+
+  }
+  const changeUnit = async () => {
+    // e.preventDefault();
+    if (Unit === "metric") {
+      setunit("imperial");
+    } else {
+      setunit("metric");
+    }
+    if (cityName) {
+      let ApiResponse = await getWeatherData(cityName, Unit);
+      setWeatherData(ApiResponse);
+    }
+  }
+
+  const toTitleCase = (str) => {
+    return str.replace(
+      /\w\S*/g,
+      function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+      }
+    );
   }
 
   return (
@@ -36,24 +62,32 @@ function App() {
             <div className="input-field col s6 center">
               <input type="text" onChange={(e) => { setcityName(e.target.value) }} />
               <label htmlFor="first_name" id="first_name" type="text" className="validate"><i className="fa-solid fa-magnifying-glass"></i>  City Name</label>
+              <div className="switch">
+                <label>
+                  Imperial
+                  <input type="checkbox" onChange={(e) => changeUnit()} />
+                  <span className="lever"></span>
+                </label>
+              </div>
             </div>
           </div>
         </form>
       </div>
-
       {weatherData && < div className="row">
         <div className="col s12 m7">
           <div className="card">
             <div className="card-image image">
               <img src={weatherIcon} />
             </div>
-            <span className="card-title title">{weatherData.name},{weatherData.sys.country}</span>
+            <span className="card-title title">{weatherData.name},{weatherData.sys.country} ({toTitleCase(weatherData.weather[0].description)})</span>
             <div className="card-content">
-              <p>Temperature: {weatherData.main.temp}</p>
-              <p>Feels Like: {weatherData.main.feels_like}</p>
-              <p>Pressure: {weatherData.main.pressure}</p>
-              <p>Humidity: {weatherData.main.humidity}</p>
+              <p>Temperature: {weatherData.main.temp} {Unit === "metric" ? " C " : " F"}</p>
+              <p>Feels Like: {weatherData.main.feels_like}{Unit === "metric" ? " C " : " F"}</p>
+              <p>Pressure: {weatherData.main.pressure} hPa</p>
+              <p>Humidity: {weatherData.main.humidity} %</p>
               <p>Visibility: {weatherData.visibility}</p>
+              <p>Wind Speed: {weatherData.wind.speed}{Unit === " metric" ? "m/s " : " miles/hr"}</p>
+              <p>Cloudiness: {weatherData.clouds.all} %</p>
             </div>
             <div className="card-action">
             </div>
@@ -61,8 +95,6 @@ function App() {
         </div>
       </div>}
     </div>
-
-
   );
 }
 
--- src/weatherCall.js
+++ src/weatherCall.js
@@ -1,9 +1,9 @@
 const APIKey = "b1ee6b0ab2f054e7a60a315e0f475813";
 const domin = "https://api.openweathermap.org";
 
-const getWeatherData = async (cityName) => {
+const getWeatherData = async (cityName, unit) => {
     console.log("Fetching weather for: ", cityName);
-    const path = `/data/2.5/weather?q=${cityName.toLowerCase()}&appid=${APIKey}`;
+    const path = `/data/2.5/weather?q=${cityName.toLowerCase()}&appid=${APIKey}&units=${unit}`;
     let weatherData = await fetch(domin + path);
     weatherData = await weatherData.json();
     return weatherData;
